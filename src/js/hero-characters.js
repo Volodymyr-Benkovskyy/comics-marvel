f9bf28b1bd109c62499884a94f4b3089cdb8d4e0
Volodymyr-Benkovskyy <123126442+Volodymyr-Benkovskyy@users.noreply.github.com>
2023-09-22 00:06:28 +0200

    add section hero-characters

---
+++ src/js/hero-characters.js
@@ -0,0 +1,37 @@
+const swiper = new Swiper('.section-hero-swiper', {
+  direction: 'vertical',
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  spaceBetween: 50,
+  slidesPerView: 1,
+
+  initialSlide: 0,
+});
+
+const swiperaPagination = document.querySelector('.swiper-pagination');
+const btnColor = document.querySelector('.js-btn-color');
+
+const onSwiper = event => {
+  const { target: bullet } = event;
+  const ariaLabel = bullet.getAttribute('aria-label');
+  let color = '';
+
+  if (ariaLabel === 'Go to slide 1') {
+    color = '#600404';
+    btnColor.style.background = color;
+  } else if (ariaLabel === 'Go to slide 2') {
+    color = '#34387f';
+    btnColor.style.background = color;
+  } else if (ariaLabel === 'Go to slide 3') {
+    color = '#5b7f3c';
+    btnColor.style.background = color;
+  }
+};
+
+swiperaPagination.addEventListener('click', onSwiper);
+
+/* swiperSlideHero */
