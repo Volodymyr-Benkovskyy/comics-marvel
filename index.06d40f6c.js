53dfe440953609e65480e812e71f301869305df2
Volodymyr-Benkovskyy <123126442+Volodymyr-Benkovskyy@users.noreply.github.com>
2023-09-22 22:38:17 +0000

    Deploying to gh-pages from @ Volodymyr-Benkovskyy/comics-marvel@4fbc60a66d60e5dc7e70b24f162e61fb503f46c0 🚀

--- index.06d40f6c.js
+++
@@ -1,3 +0,0 @@
-new Swiper(".section-hero-swiper",{direction:"vertical",loop:!0,pagination:{el:".swiper-pagination",clickable:!0},spaceBetween:50,slidesPerView:1,initialSlide:0});const e=document.querySelector(".swiper-pagination"),t=document.querySelector(".js-btn-color");e.addEventListener("click",e=>{let{target:i}=e,o=i.getAttribute("aria-label"),l="";"Go to slide 1"===o?(l="#600404",t.style.background=l):"Go to slide 2"===o?(l="#34387f",t.style.background=l):"Go to slide 3"===o&&(l="#5b7f3c",t.style.background=l)});/* swiperSlideHero *///# sourceMappingURL=index.06d40f6c.js.map
-
-//# sourceMappingURL=index.06d40f6c.js.map
