27b1acbce5540040ee3f21e42254a0d0c9cc55ec
Volodymyr-Benkovskyy <123126442+Volodymyr-Benkovskyy@users.noreply.github.com>
2023-09-26 17:33:02 +0200

    add touch .gitattributes

--- src/js/rendom-characters.js
+++ src/js/rendom-characters.js
@@ -1,5 +1,5 @@
 import { api } from './apiMarvel';
-import { showNextSlide } from './helpers/slide';
+//import { showNextSlide } from './helpers/slide';
 const randomImg = document.querySelector('.js-random-img');
 const randomDescr = document.querySelector('.js-random-descr');
 
@@ -79,3 +79,32 @@ const getRandomCharacters = async () => {
   }
 };
 getRandomCharacters();
+
+let slideImagesArray = null;
+let slideDescriptionsArray = null;
+
+let slideIndex = 0;
+let imageIndex = 0;
+
+const showNextSlide = () => {
+  for (let i = 0; i < slideImagesArray.length; i += 1) {
+    slideImagesArray[i].classList.remove('js-show');
+    slideImagesArray[i].classList.add('js-slide');
+  }
+
+  for (let i = 0; i < slideDescriptionsArray.length; i += 1) {
+    slideDescriptionsArray[i].classList.remove('js-active');
+    slideDescriptionsArray[i].classList.add('js-random-characters-description');
+  }
+
+  imageIndex = (imageIndex + 1) % slideImagesArray.length;
+  slideIndex = (slideIndex + 1) % slideDescriptionsArray.length;
+
+  slideImagesArray[imageIndex].classList.add('js-show');
+  slideImagesArray[imageIndex].classList.remove('js-slide');
+
+  slideDescriptionsArray[slideIndex].classList.add('js-active');
+  slideDescriptionsArray[slideIndex].classList.remove(
+    'js-random-characters-description'
+  );
+};
