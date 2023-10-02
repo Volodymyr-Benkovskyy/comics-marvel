4b13cf89c274186653b732c786d2d25b475d92bd
Volodymyr-Benkovskyy <123126442+Volodymyr-Benkovskyy@users.noreply.github.com>
2023-10-02 18:50:29 +0200

    added function onSearchComics and  onSearchName

--- README.md
+++ README.md
@@ -23,3 +23,4 @@
 <!-- add section sort-characters html, css,svg,  tui-pagination-->
 <!-- add function getComics sort-characters.js -->
 <!-- add function fetchAndRenderCharacterList,createSortContainer  -->
+<!-- added function onSearchComics and  onSearchName  -->
--- src/js/partialsJs/sort-characters.js
+++ src/js/partialsJs/sort-characters.js
@@ -1,18 +1,22 @@
 import { api } from './apiMarvel';
 import { showLoader, hideLoader } from '../helpers/loader';
 import { getItemsPerPage } from '../helpers/getItemsPerPage.js';
+import debounce from 'lodash.debounce';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
 
 const sortContainer = document.querySelector('.js-characters-sort-container');
 const formSearchFilter = document.querySelector('.js-characters-sort-form');
+const listScroll = document.getElementById('characters-list');
 const inputComicsEl = document.querySelector('.js-chararcters-comics-input');
+const selectOrderEl = document.querySelector('.js-characters-select-order');
 const inputNameEl = document.querySelector('.js-chararcters-name-input');
+const formHeaderEl = document.querySelector('.js-header-chararcters-name');
+const inputHeaderEl = document.querySelector('.js-header-chararcters-input');
 
 let idComics = [];
-let queryComics = null;
-let queryIdComics = null;
 let itemsPerPage = null;
-
-// Масив для зберігання ідентифікаторів коміксів.
+let queryName = null;
 itemsPerPage = getItemsPerPage();
 
 const createSortContainer = data => {
@@ -43,28 +47,73 @@ const renderSortContainerList = data => {
   sortContainer.insertAdjacentHTML('beforeend', createSortContainer(data));
 };
 
+/* /////////////////////////////////////////////////// */
+
+const boxPagination = document.querySelector('.tui-pagination');
+const optionsPagination = {
+  totalItems: 0,
+  itemsPerPage: itemsPerPage,
+  visiblePages: 3,
+};
+const pagination = new Pagination(boxPagination, optionsPagination);
+
+/* ////////////////////     RANDOM   ////////////////////////////// */
+
 const fetchAndRenderCharacterList = async () => {
   try {
     showLoader;
     const response = await api.getCharacters({
-      limit: itemsPerPage,
       offset: 0,
+      limit: itemsPerPage,
     });
+    console.log('fetchAndRenderCharacterList =>', response);
     hideLoader();
     renderSortContainerList(response.results);
+    boxPagination.classList.remove('is-hidden'); //  Показуємо пагінацію
+
+    if (itemsPerPage > response.total) {
+      return;
+    }
+    pagination.reset(response.total);
+    pagination.on('beforeMove', async event => {
+      const currentPage = event.page;
+      let offset = itemsPerPage * (currentPage - 1);
+
+      try {
+        showLoader();
+        const newResponse = await api.getCharacters({
+          offset: offset,
+          limit: itemsPerPage,
+          nameStartsWith: queryName,
+        });
+
+        hideLoader();
+        clearSortContainer();
+        renderSortContainerList(newResponse.results);
+      } catch (error) {
+        console.log('Error');
+        hideLoader();
+      }
+    });
   } catch (error) {
     console.log(error);
   }
 };
 fetchAndRenderCharacterList();
 
-/* //////////////////////////////////////////////////// */
+/* ////////////////////    / RANDOM  /  ////////////////////////////// */
 
-const onSearchCharacters = async event => {
+/* ////////////////     SEARCH-INPUT-COMICS   //////////////////////////////////// */
+const onSearchComics = async event => {
   showLoader();
   event.preventDefault();
   const { target: formEl } = event;
   queryComics = formEl.elements.searchComics.value;
+
+  idComics = [];
+  queryName = null;
+  queryOrder = null;
+  dateQuery = null;
 
   try {
     const response = await api.getComics({
@@ -73,44 +122,91 @@ const onSearchCharacters = async event => {
       limit: 100,
     });
 
-    // Перевіряємо, чи були знайдені комікси за заданим запитом.
     if (response.total === 0) {
       hideLoader();
+
       inputComicsEl.value = '';
+      boxPagination.classList.add('is-hidden');
       return;
     }
 
-    // Отримуємо перші 10 коміксів з результатів пошуку.
     const limitedResults = response.results.slice(0, 10);
-
-    // Додаємо ідентифікатори коміксів до масиву idComics, якщо вони містять персонажів.
     limitedResults.map(el => {
       if (el.characters.available) {
         idComics.push(el.id);
+      } else {
+        return;
       }
-      return;
     });
+    console.log('onSearchComics => getComics=>', response);
+    arrayIdComics = idComics.join(', ');
 
-    // Об'єднуємо ідентифікатори коміксів в рядок через кому.
-    queryIdComics = idComics.join(', ');
-
-    // Виконуємо запит до API для отримання персонажів, які з'являються в обраних коміксах.
     try {
       const response = await api.getCharacters({
-        offset: offset,
         nameStartsWith: queryName,
-        comics: queryIdComics,
-        orderBy: queryOrder,
-        modifiedSince: dateQuery,
+        comics: arrayIdComics,
+        limit: itemsPerPage,
       });
-      console.log('queryName =>', queryName);
-    } catch (error) {}
 
-    hideLoader();
+      if (response.length === 0) {
+        boxPagination.classList.add('is-hidden');
+
+        return;
+      }
+      hideLoader();
+      clearSortContainer();
+      pagination.reset(response.total);
+      renderSortContainerList(response.results);
+      boxPagination.classList.remove('is-hidden');
+
+      inputComicsEl.value = '';
+      console.log('onSearchComics => getCharacters', response);
+    } catch (error) {
+      console.log('Error!', error);
+    }
   } catch (error) {
     hideLoader();
-    console.log(error);
+    console.log('Error!', error);
   }
 };
 
-formSearchFilter.addEventListener('submit', onSearchCharacters);
+formSearchFilter.addEventListener('submit', onSearchComics);
+
+/* ////////////////    / SEARCH-INPUT-COMICS  /   //////////////////////////////////// */
+
+/* ///////////////////   SEARCH-NAME  ///////////////////////////////////// */
+
+const onSearchName = async event => {
+  boxPagination.classList.add('is-hidden');
+  queryName = event.target.value;
+
+  try {
+    showLoader();
+    const response = await api.getCharacters({
+      offset: 0,
+      limit: itemsPerPage,
+      nameStartsWith: queryName,
+    });
+
+    if (response.total === 0) {
+      hideLoader();
+      boxPagination.classList.add('is-hidden');
+      return;
+    }
+
+    console.log('onSearchName =>response', response);
+    hideLoader();
+
+    pagination.reset(response.total);
+    clearSortContainer();
+    renderSortContainerList(response.results);
+    boxPagination.classList.remove('is-hidden');
+    inputNameEl.value = '';
+  } catch (error) {
+    console.log('Error!', error);
+  }
+};
+
+inputNameEl.addEventListener('input', debounce(onSearchName, 2000));
+
+/* ////////////////    /SEARCH-NAME  /   //////////////////////////////////// */
